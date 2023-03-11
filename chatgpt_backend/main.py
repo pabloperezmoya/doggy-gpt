da0daaf7433655aeb1550cac8ce2b02477bac61e
Pablo <ppmoya08@gmail.com>
2023-03-10 23:25:29 -0300

    Modified trusted hosts

--- chatgpt_backend/main.py
+++ chatgpt_backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from routes import chat
 from routes import user
 
@@ -9,9 +9,11 @@ app.include_router(chat.router)
 app.include_router(user.router)
 
 origins = [
-    "https://doggy-gpt-frontend.vercel.app/"
+    "https://doggy-gpt-frontend.vercel.app"
 ]
-
+app.add_middleware(
+    TrustedHostMiddleware, allowed_hosts=["https://doggy-gpt-frontend.vercel.app"] 
+)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
